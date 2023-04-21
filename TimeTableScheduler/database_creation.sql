90579bf0c07e3c74bae260ff57984a6be486f041
Mihai Cristian-Laurentiu <mihai.laurentiu24.ml@gmail.com>
2023-04-21 11:54:53 +0300

    Fixed database types

--- TimeTableScheduler/database_creation.sql
+++ TimeTableScheduler/database_creation.sql
@@ -28,7 +28,7 @@ CREATE TABLE TimeSlots (
     weekday TEXT NOT NULL,
     discipline_id INTEGER NOT NULL,
     teacher_id INTEGER NOT NULL,
-    students_id TEXT NOT NULL,
+    students_id INTEGER NOT NULL,
     FOREIGN KEY (discipline_id) REFERENCES Disciplines(id),
     FOREIGN KEY (teacher_id) REFERENCES Teachers(id),
     FOREIGN KEY (students_id) REFERENCES Students(id)
