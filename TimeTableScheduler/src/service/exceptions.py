66466f827e1a4a60ae1b949af555a99aa9b7fd37
Valentin Stamate <stamatevalentin125@gmail.com>
2023-04-21 23:52:33 +0300

    Create base functionality for html service

---
+++ TimeTableScheduler/src/service/exceptions.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+
+class Exceptions(Enum):
+    INVALID_ROW = 'Invalid row'
---
+++ TimeTableScheduler/src/service/timetable_service.py
@@ -0,0 +1,28 @@
+
+
+class StudentTimetableGenerator:
+    """ Generates a timetable page with study years and groups """
+
+    @staticmethod
+    def generate():
+        pass
+
+
+class TeachersTimetableGenerator:
+
+    @staticmethod
+    def generate():
+        pass
+
+
+class ClassesTimetableGenerator:
+
+    @staticmethod
+    def generate():
+        pass
+
+
+class RoomsTimetableGenerator:
+    @staticmethod
+    def generate():
+        pass
