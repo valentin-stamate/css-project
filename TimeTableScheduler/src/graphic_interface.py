4050fdf445bea58fe2dabc295f3d34c8f96b1069
Alin Ursu <alinstefanursu@gmail.com>
2023-04-22 15:49:33 +0300

    Show db tables in UI

--- TimeTableScheduler/src/graphic_interface.py
+++ TimeTableScheduler/src/graphic_interface.py
@@ -1,15 +1,18 @@
 import tkinter as tk
 from tkinter import ttk
 
+from src.database_connection import DatabaseConnection
+
 
 class SchedulerApp:
     WIDTH = 1200
+    HEIGHT = 600
     TABS = 5
 
     def __init__(self, master):
         self.master = master
         self.master.title("Scheduler App")
-        self.notebook = ttk.Notebook(self.master, width=SchedulerApp.WIDTH)
+        self.notebook = ttk.Notebook(self.master, width=SchedulerApp.WIDTH, height=SchedulerApp.HEIGHT)
         self.notebook.pack(fill="both", expand=True)
         self.current_tab = None
         style = ttk.Style()
@@ -17,21 +20,28 @@ class SchedulerApp:
         self.notebook.configure(style="myStyle.TNotebook")
 
         # create tabs
-        self.create_tab("Students")
-        self.create_tab("Teachers")
-        self.create_tab("Disciplines")
-        self.create_tab("Rooms")
-        self.create_tab("Schedule")
+        self.create_tab_and_display_db_table("Student Groups", "StudentGroups")
+        self.create_tab_and_display_db_table("Teachers", "Teachers")
+        self.create_tab_and_display_db_table("Disciplines", "Disciplines")
+        self.create_tab_and_display_db_table("Rooms", "Rooms")
+        self.create_tab_and_display_db_table("Schedules", "TimeSlots")
 
-    def create_tab(self, name):
-        # create a new tab with a text widget for content
+    def create_tab_and_display_db_table(self, name: str, table: str):
         tab = tk.Frame(self.notebook)
         tab.pack(side="top")
-        label = tk.Label(tab, width=40, height=30)
-        label.pack(side="top")
-        inner_frame = tk.Frame(tab, height=300)
+
+        inner_frame = tk.Frame(tab)
         inner_frame.pack()
-        # add the tab to the tabs list and select it
+
+        rows = DatabaseConnection.get_instance().get_all_rows(table)
+        number_of_columns = len(rows[0]) if len(rows) > 0 else 1
+
+        tree = ttk.Treeview(inner_frame, columns=[f"c{i + 1}" for i in range(0, number_of_columns)], show='headings',
+                            height=(SchedulerApp.HEIGHT - 50))
+        for row in rows:
+            tree.insert("", tk.END, values=row)
+        tree.pack()
+
         self.notebook.add(tab, text=f"{name}")
         self.select_tab(tab)
 
