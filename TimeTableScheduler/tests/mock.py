6bb6a3ffcbe69a7a9064998140bcfbf0188ffdf3
Valentin Stamate <stamatevalentin125@gmail.com>
2023-04-22 16:17:41 +0300

    Timetables for students

--- TimeTableScheduler/tests/mock.py
+++ TimeTableScheduler/tests/mock.py
@@ -1,2 +1,25 @@
+from typing import List
 
-MOCK_STUDENT_TIMETABLE = '<title>Document</title></head><body><h1>Timetable test</h1><div><strong>Generated: 2023-04-21</strong></div><hr><table><tr><th colspan="1">Head 1</th><th colspan="1">Head 2</th><th colspan="1">Head 3</th><th colspan="1">Head 4</th><th colspan="1">Head 5</th></tr><tr><td colspan="5">Day 1</td></tr><tr><td colspan="1">12</td><td colspan="1">14</td><td colspan="1">Name</td><td colspan="1">Author</td><td colspan="1">C102</td></tr><tr><td colspan="5">Day 2</td></tr><tr><td colspan="1">12</td><td colspan="1">14</td><td colspan="1">Name</td><td colspan="1">Author</td><td colspan="1">C102</td></tr></table>'
+from src.service.timetable_service import ProgrammedClass
+
+MOCK_STUDENT_TIMETABLE = '<table><tr><th colspan="1">Head 1</th><th colspan="1">Head 2</th><th colspan="1">Head 3</th><th colspan="1">Head 4</th><th colspan="1">Head 5</th></tr><tr><td colspan="5">Day 1</td></tr><tr><td colspan="1">12</td><td colspan="1">14</td><td colspan="1">Name</td><td colspan="1">Author</td><td colspan="1">C102</td></tr><tr><td colspan="5">Day 2</td></tr><tr><td colspan="1">12</td><td colspan="1">14</td><td colspan="1">Name</td><td colspan="1">Author</td><td colspan="1">C102</td></tr></table>'
+
+TIMETABLE_MOCK: List[ProgrammedClass] = [
+    ProgrammedClass(day="Monday", to="10:00", from_="8:00", group=1, group_class="A1", year=1, class_name="Introduction to Programming", class_type="Lecture", professors=["Smith", "Jones"], rooms=["A101"], frequency="Weekly"),
+    ProgrammedClass(day="Monday", to="12:00", from_="10:00", group=2, group_class="B1", year=1, class_name="Introduction to Programming", class_type="Lecture", professors=["Smith", "Jones"], rooms=["A101"], frequency="Weekly"),
+    ProgrammedClass(day="Monday", to="14:00", from_="12:00", group=1, group_class="A2", year=1, class_name="Discrete Mathematics", class_type="Lecture", professors=["Brown"], rooms=["B201"], frequency="Weekly"),
+    ProgrammedClass(day="Monday", to="16:00", from_="14:00", group=2, group_class="B1", year=1, class_name="Discrete Mathematics", class_type="Lecture", professors=["Brown"], rooms=["B201"], frequency="Weekly"),
+    ProgrammedClass(day="Tuesday", to="10:00", from_="8:00", group=1, group_class="A1", year=1, class_name="Introduction to Programming", class_type="Lab", professors=["Green"], rooms=["C301"], frequency="Biweekly"),
+    ProgrammedClass(day="Tuesday", to="12:00", from_="10:00", group=2, group_class="B1", year=2, class_name="Introduction to Programming", class_type="Lab", professors=["Green"], rooms=["C301"], frequency="Biweekly"),
+    ProgrammedClass(day="Tuesday", to="14:00", from_="12:00", group=3, group_class="A2", year=2, class_name="Data Structures and Algorithms", class_type="Lecture", professors=["Lee"], rooms=["A102"], frequency="Weekly"),
+    ProgrammedClass(day="Tuesday", to="16:00", from_="14:00", group=4, group_class="B2", year=2, class_name="Data Structures and Algorithms", class_type="Lecture", professors=["Lee"], rooms=["A102"], frequency="Weekly"),
+    ProgrammedClass(day="Wednesday", to="10:00", from_="8:00", group=3, group_class="A1", year=2, class_name="Data Structures and Algorithms", class_type="Lab", professors=["Wang"], rooms=["C302"], frequency="", package='A'),
+    ProgrammedClass(day="Wednesday", to="12:00", from_="10:00", group=4, group_class='B2', year=2, class_name='Data Structures and Algorithms', class_type='Lab', professors=['Wang'], rooms=['C302'], frequency='', package='A'),
+    ProgrammedClass(day='Wednesday', to='14:00', from_='12:00', group=3, group_class='A2', year=3, class_name='Operating Systems', class_type='Lecture', professors=['Chen'], rooms=['B202'], frequency='', package='B'),
+    ProgrammedClass(day='Wednesday', to='16:00', from_='14:00', group=4, group_class='B2', year=3, class_name='Operating Systems', class_type='Lecture', professors=['Chen'], rooms=['B202'], frequency='', package='B'),
+    ProgrammedClass(day='Thursday', to='10:00', from_='8:00', group=3, group_class='A2', year=2, class_name='Operating Systems', class_type='Lab', professors=['Kim'], rooms=['C303'], frequency='', package='B'),
+    ProgrammedClass(day='Thursday', to='12:00', from_='10:00', group=4, group_class='B2', year=2, class_name='Operating Systems', class_type='Lab', professors=['Kim'], rooms=['C303'], frequency='', package='B'),
+    ProgrammedClass(day='Thursday', to='14:00', from_='12:00', group=3, group_class='A2', year=2, class_name='Database Systems', class_type='Lecture', professors=['Singh'], rooms=['A103'], frequency='', package='A'),
+    ProgrammedClass(day='Friday', to='14:00', from_='12:00', group=3, group_class='A2', year=2, class_name='Database Systems', class_type='Lecture', professors=['Singh'], rooms=['A103'], frequency='', package='A'),
+    ProgrammedClass(day='Friday', to='14:00', from_='12:00', group=3, group_class='A4', year=2, class_name='Database Systems', class_type='Lecture', professors=['Singh'], rooms=['A103'], frequency='', package='A'),
+]
--- TimeTableScheduler/tests/service/html_service_test.py
+++ TimeTableScheduler/tests/service/html_service_test.py
@@ -1,23 +1,53 @@
 import unittest
 
 from src.service.html_service import TimetablePage
-from tests.mock import MOCK_STUDENT_TIMETABLE
+from src.service.timetable_service import TimetableGenerator
+from tests.mock import MOCK_STUDENT_TIMETABLE, TIMETABLE_MOCK
 
 
 class TestTimetablePage(unittest.TestCase):
+    pass
+    # def test_generate_html(self):
+    #     title = "Timetable test"
+    #     data = [
+    #         ['Head 1', 'Head 2', 'Head 3', 'Head 4', 'Head 5'],
+    #         ['Day 1'],
+    #         ['12', '14', 'Name', 'Author', 'C102'],
+    #         ['Day 2'],
+    #         ['12', '14', 'Name', 'Author', 'C102'],
+    #     ]
+    #
+    #     timetable = TimetablePage(title, data, 5, 'test_timetable.html')
+    #     html = timetable.generate_html()
+    #
+    #     assert title in html
+    #     assert MOCK_STUDENT_TIMETABLE in html
 
-    def test_generate_html(self):
-        title = "Timetable test"
-        data = [
-            ['Head 1', 'Head 2', 'Head 3', 'Head 4', 'Head 5'],
-            ['Day 1'],
-            ['12', '14', 'Name', 'Author', 'C102'],
-            ['Day 2'],
-            ['12', '14', 'Name', 'Author', 'C102'],
-        ]
 
-        timetable = TimetablePage(title, data, 5)
-        html = timetable.generate_html()
+class TestTimetableService(unittest.TestCase):
 
-        assert title in html
-        assert MOCK_STUDENT_TIMETABLE in html
+
+    def test_main_page_generation(self):
+        timetable_service = TimetableGenerator(TIMETABLE_MOCK)
+
+        timetable_service.generate_main_page()
+
+    def test_students_page_generation(self):
+        timetable_service = TimetableGenerator(TIMETABLE_MOCK)
+
+        timetable_service.generate_student_page()
+
+    def test_professors_page_generation(self):
+        timetable_service = TimetableGenerator(TIMETABLE_MOCK)
+
+        timetable_service.generate_professors_page()
+
+    def test_rooms_page_generation(self):
+        timetable_service = TimetableGenerator(TIMETABLE_MOCK)
+
+        timetable_service.generate_rooms_page()
+
+    def test_classes_page_generation(self):
+        timetable_service = TimetableGenerator(TIMETABLE_MOCK)
+
+        timetable_service.generate_classes_page()
