e94aefdb1220827d009989e6f75d47e1c8a2e4ea
Valentin Stamate <stamatevalentin125@gmail.com>
2023-05-13 23:10:54 +0300

    Test html service

--- TimeTableScheduler/test/service/test_html_service.py
+++ TimeTableScheduler/test/service/test_html_service.py
@@ -1,43 +1,87 @@
+import datetime
 import unittest
+from unittest.mock import patch, mock_open, MagicMock
 
+from src.service.html_service import HTMLPage, TimetablePage
 from src.service.timetable_service import TimetableGenerator
 from test.const.mock import TIMETABLE_MOCK
 
 
+class TestHTMLPage(unittest.TestCase):
+
+    def test_add(self):
+        html_page = HTMLPage()
+
+        self.assertEqual(len(html_page.elements), 0)
+        html_page.add('')
+        self.assertEqual(len(html_page.elements), 1)
+
+    def test_generate_html(self):
+        html_page = HTMLPage()
+        html_page.header = '<html><body>'
+
+        html_page.add('<span>test</span>')
+
+        result = html_page.generate_html()
+
+        self.assertEqual(result, '<html><body><span>test</span></body></html>')
+
+
 class TestTimetablePage(unittest.TestCase):
 
-    def test_demo(self):
+    @classmethod
+    def setUpClass(cls):
         pass
 
+    @classmethod
+    def tearDownClass(cls):
+        pass
 
-class TestTimetableService(unittest.TestCase):
+    def test_generate_html(self):
+        mock_title = 'Title'
+        mock_headers = ['id', 'firstname', 'lastname']
+        mock_data = [[1, 'john', 'doe'], [2, 'lorm', ['ipsum', 'sit']]]
+        mock_path = ''
 
-    def test_generate_all_timetables(self):
-        timetable_service = TimetableGenerator(TIMETABLE_MOCK)
+        HTMLPage.header = ''
+        HTMLPage.closing_header = ''
 
-        timetable_service.generate_all()
+        mock_file_open = mock_open()
 
-    def test_main_page_generation(self):
-        timetable_service = TimetableGenerator(TIMETABLE_MOCK)
+        with patch('builtins.open', mock_file_open):
+            timetable_page = TimetablePage(mock_title, mock_headers, mock_data, mock_path)
 
-        timetable_service.generate_main_page()
+            self.assertEqual(timetable_page.title, mock_title)
+            self.assertEqual(type(timetable_page.date) is datetime.date, True)
+            self.assertEqual(timetable_page.headers, mock_headers)
+            self.assertEqual(timetable_page.data, mock_data)
+            self.assertEqual(timetable_page.cols, len(mock_headers))
+            self.assertEqual(timetable_page.path, mock_path)
 
-    def test_students_page_generation(self):
-        timetable_service = TimetableGenerator(TIMETABLE_MOCK)
+            result = timetable_page.generate_html()
 
-        timetable_service.generate_student_page()
+            mock_file_open.assert_called_once_with(mock_path, 'wt')
+            handle = mock_file_open()
+            handle.close.assert_called_once()
 
-    def test_professors_page_generation(self):
-        timetable_service = TimetableGenerator(TIMETABLE_MOCK)
-
-        timetable_service.generate_professors_page()
-
-    def test_rooms_page_generation(self):
-        timetable_service = TimetableGenerator(TIMETABLE_MOCK)
-
-        timetable_service.generate_rooms_page()
-
-    def test_classes_page_generation(self):
-        timetable_service = TimetableGenerator(TIMETABLE_MOCK)
-
-        timetable_service.generate_classes_page()
+            self.assertEqual(result, '<h1>Title</h1>'
+                                     '<div>'
+                                     '<strong>Generated: 2023-05-13</strong></div>'
+                                     '<hr>'
+                                     '<table>'
+                                        '<tr>'
+                                            '<th colspan="1">id</th>'
+                                            '<th colspan="1">firstname</th>'
+                                            '<th colspan="1">lastname</th>'
+                                        '</tr>'
+                                        '<tr>'
+                                            '<td colspan="1">1</td>'
+                                            '<td colspan="1">john</td>'
+                                            '<td colspan="1">doe</td>'
+                                        '</tr>'
+                                        '<tr>'
+                                            '<td colspan="1">2</td>'
+                                            '<td colspan="1">lorm</td>'
+                                            '<td colspan="1">ipsum, sit</td>'
+                                        '</tr>'
+                                     '</table>')
